81be72d3b290e6a3e40518761d883f6e16184a54
Antony Leons <antonyleons@qburst.com>
2023-08-14 17:09:35 +0530

    initial commit

---
+++ lib/fastlane/plugin/gmail_notify/actions/gmail_notify_action.rb
@@ -0,0 +1,108 @@
+require "fastlane/action"
+require_relative "../helper/gmail_notify_helper"
+
+module Fastlane
+  module Actions
+    class GmailNotifyAction < Action
+      def self.run(params)
+
+        require 'pony'
+
+        release_notes = File.read(params[:release_notes_file]).gsub("*", "<li>").gsub("\n", "</li>")
+
+        eth = Fastlane::ErbTemplateHelper
+        html_template = eth.load_from_path(params[:template_file])
+
+        body = eth.render(html_template, params[:placeholders])
+
+        Pony.mail({
+          :to => params[:recipients],
+          :cc => params[:cc],
+          :subject => params[:subject],
+          :html_body => body,
+          :body_part_header => { content_disposition: "inline" },
+          :via => :smtp,
+          :via_options => {
+            :address => "smtp.gmail.com",
+            :port => "587",
+            :enable_starttls_auto => true,
+            :user_name => params[:sender_email],
+            :password => params[:sender_password],
+            :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
+            :domain => "localhost.localdomain", # the HELO domain provided by the client to the server
+          },
+        })
+      end
+
+      def self.description
+        "Sends a mail using SMTP"
+      end
+
+      def self.authors
+        ["Antony Leons"]
+      end
+
+      def self.return_value
+        # If your method provides a return value, you can describe here what it does
+      end
+
+      def self.details
+        # Optional:
+        "This plugin sends a mail using SMTP"
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :sender_email,
+                                       env_name: "GMAIL_NOTIFY_SENDER_EMAIL",
+                                       description: "Gmail account to login with",
+                                       optional: false,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :sender_password,
+                                       env_name: "GMAIL_NOTIFY_SENDER_PASSWORD",
+                                       description: "Password of Gmail account to login with",
+                                       optional: false,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :recipients,
+                                       env_name: "GMAIL_NOTIFY_RECIPIENTS",
+                                       description: "Recipients of email",
+                                       optional: false,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :cc,
+                                       env_name: "GMAIL_NOTIFY_CC",
+                                       description: "CC of email",
+                                       optional: true,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :subject,
+                                       env_name: "GMAIL_NOTIFY_SUBJECT",
+                                       description: "Subject of email",
+                                       optional: false,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :template_file,
+                                       env_name: "GMAIL_NOTIFY_TEMPLATE_FILE",
+                                       description: "Path of template file",
+                                       optional: false,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :release_notes_file,
+                                       env_name: "GMAIL_NOTIFY_RELEASE_NOTES_FILE",
+                                       description: "Path of release notes file",
+                                       optional: false,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :placeholders,
+                                       env_name: "GMAIL_NOTIFY_TEMPLATE_PLACEHOLDERS",
+                                       description: "Placeholders for email template",
+                                       optional: false,
+                                       type: Hash),
+        ]
+      end
+
+      def self.is_supported?(platform)
+        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
+        # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
+        #
+        # [:ios, :mac, :android].include?(platform)
+        true
+      end
+    end
+  end
+end
---
+++ lib/fastlane/plugin/gmail_notify/helper/gmail_notify_helper.rb
@@ -0,0 +1,52 @@
+require "fastlane_core/ui/ui"
+require "erb"
+
+module Fastlane
+  UI = FastlaneCore::UI unless Fastlane.const_defined?("UI")
+
+  module Helper
+    class GmailNotifyHelper
+      # class methods that you define here become available in your action
+      # as `Helper::GmailNotifyHelper.your_method`
+      #
+      def self.show_message
+        UI.message("Hello from the gmail_notify plugin helper!")
+      end
+    end
+
+    class ErbTemplateHelper
+      def self.load(template_name)
+        path = "#{Fastlane::ROOT}/lib/assets/#{template_name}.erb"
+        load_from_path(path)
+      end
+
+      def self.load_from_path(template_filepath)
+        unless File.exist?(template_filepath)
+          UI.user_error!("Could not find template at path '#{template_filepath}'")
+        end
+        File.read(template_filepath)
+      end
+
+      def self.render(template, template_vars_hash, trim_mode = nil)
+        Fastlane::Helper::ErbalT.new(template_vars_hash, trim_mode).render(template)
+      end
+    end
+
+    class ErbalT < OpenStruct
+      def initialize(hash, trim_mode = nil)
+        super(hash)
+        @trim_mode = trim_mode
+      end
+
+      def render(template)
+        # From Ruby 2.6, ERB.new takes keyword arguments and positional ones are deprecated
+        # https://bugs.ruby-lang.org/issues/14256
+        if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.6.0")
+          ERB.new(template, trim_mode: @trim_mode).result(binding)
+        else
+          # ERB.new(template, nil, @trim_mode).result(binding)
+        end
+      end
+    end
+  end
+end
